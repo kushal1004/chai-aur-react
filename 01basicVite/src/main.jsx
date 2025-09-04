5f502ee374cb9ffe7166fd581acf666fec28ee28
Kushal Singha <kushalsingha00@gmail.com>
2025-09-04 21:54:46 +0530

    practice using create custom react

--- 01basicVite/src/main.jsx
+++ 01basicVite/src/main.jsx
@@ -1,9 +1,25 @@
 import { StrictMode } from 'react'
-import  { createRoot }  from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import React from 'react'
 import App from './App.jsx'
 
+export function MyApp () {
+  return <h1>Hello kushal Singha</h1>
+}
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  'Click me'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <App />
-  </StrictMode>,
+    <MyApp />
+    {reactElement}
+  </>
 )
---
+++ customReact/customReact.js
@@ -0,0 +1,25 @@
+function CustomeReact (element, container) {
+  const domElement = document.createElement(element.type)
+  domElement.innerHTML = element.Children
+  for (const prop in element.props) {
+    if (prop === 'children') continue
+    domElement.setAttribute(prop, element.props[prop])
+  }
+  
+  // domElement.setAttribute('href', element.props.href)
+  // domElement.setAttribute('target',element.props.target)
+
+  container.appendChild(domElement)
+}
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  Children: 'Click me'
+}
+
+const mainContainer = document.getElementById('root')
+
+CustomeReact(reactElement, mainContainer)
